02af605fe2e900e217ccd5cf3f086337f3e9d641
Yasin Boulahya Chairi <YBC5291@cemosa.es>
2022-04-26 19:12:15 +0200

    Resource Models

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 sap.ui.require([
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/core/mvc/XMLView",
-	"sap/ui/model/BindingMode"
-], function (JSONModel, XMLView, BindingMode) {
+	"sap/ui/model/resource/ResourceModel"
+], function (JSONModel, XMLView, ResourceModel) {
 	"use strict";
 
 	// Attach an anonymous function to the SAPUI5 'init' event
@@ -11,21 +11,31 @@ sap.ui.require([
 		var oModel = new JSONModel({
 			firstName: "Harry",
 			lastName: "Wanks",
-			enabled: true,
-			panelHeaderText: "Data Binding Basics"
-
+			enabled: true
 		});
-		
-		oModel.setDefaultBindingMode(BindingMode.OneWay);
 
-		
 		// Assign the model object to the SAPUI5 core
 		sap.ui.getCore().setModel(oModel);
+
+		// Create a resource bundle for language specific texts
+		// the configured supportedLocales represent the i18n files present:
+		// * "" - i18n/i18n.properties
+		// the configured fallbackLocale should represent one of these files
+		// * "" - according to the fallback chain the root bundle is the last fallback.
+		//   Configuring it explicitly avoids side effects when additional resource files are added.
+		// @see https://ui5.sap.com/#/topic/ec753bc539d748f689e3ac814e129563
+		var oResourceModel = new ResourceModel({
+			bundleName: "sap.ui.demo.db.i18n.i18n",
+			supportedLocales: ["", "de"],
+			fallbackLocale: ""
+		});
+
+		// Assign the model object to the SAPUI5 core using the name "i18n"
+		sap.ui.getCore().setModel(oResourceModel, "i18n");
 
 		// Display the XML view called "App"
 		new XMLView({
 			viewName: "sap.ui.demo.zybc_db.view.App"
 		}).placeAt("content");
-
 	});
 });
