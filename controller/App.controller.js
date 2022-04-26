291898661fadcbe9939a58338e626579728302f6
Yasin Boulahya Chairi <YBC5291@cemosa.es>
2022-04-26 17:35:22 +0200

    No Data Binding

---
+++ controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("demo.zybc_db.controller.App", {
+		onInit: function () {
+
+		}
+	});
+});
